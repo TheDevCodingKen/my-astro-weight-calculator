3975688afa9b3a6bc068ad9965b1d1ea3a1f6c76
TheDevCodingKen <93670654+TheDevCodingKen@users.noreply.github.com>
2022-05-13 16:58:22 -0700

    Added planetsArray and updated select element

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Write your JavaScript code here!
+const planetsArray = [
+    //Our solar system's bodies' names and their ratio to Earth's gravity
+    ["Pluto", 0.06],
+    ["Neptune", 1.148],
+    ["Uranus", 0.917],
+    ["Saturn", 1.139],
+    ["Jupiter", 2.64],
+    ["Mars", 0.3895],
+    ["Moon", 0.1655],
+    ["Earth", 1],
+    ["Venus", 0.9032],
+    ["Mercury", 0.377],
+    ["Sun", 27.9],
+];
