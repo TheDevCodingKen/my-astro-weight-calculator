195581a32c28a6f4c95d3d5280297c661e3df3a4
TheDevCodingKen <93670654+TheDevCodingKen@users.noreply.github.com>
2022-05-16 11:36:34 -0700

    Added dropdown element for planetsArray

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById('generate').onclick = function() {
+
 const planetsArray = [
     //Our solar system's bodies' names and their ratio to Earth's gravity
     ["Pluto", 0.06],
@@ -12,3 +14,28 @@ const planetsArray = [
     ["Mercury", 0.377],
     ["Sun", 27.9],
 ];
+
+let select = document.createElement("select");
+    select.name = "planets";
+    select.id = "planets"
+
+for (const planet of planetsArray){
+    var option = document.createElement("option");
+    option.value = planetsArray[i][0];
+    option.text = val.charAt(0).toUpperCase() + val.slice(1);
+    select.appendChild(option);
+}
+
+var label = document.createElement("label");
+    label.innerHTML = "Choose a Planet: ";
+    label.htmlFor = "planets";
+
+    document.getElementById("container").appendChild(label).appendChild(select);
+}
+function calculateWeight(weight, planetName){
+let weight = 100;
+for(let i = 0; i < planetsArray.length; i++) {
+    //   console.log("Your weight on" + planetsArray[i][0] + weight * planetsArray[i][1])
+       console.log(`Your weight on ${planetsArray[i][0]} is ${(weight * planetsArray[i][1]).toFixed(2)}`)
+}
+}
