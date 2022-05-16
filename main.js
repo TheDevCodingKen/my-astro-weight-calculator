71ede3589b3fed8db58658bbef99eeaf0923e8ee
TheDevCodingKen <93670654+TheDevCodingKen@users.noreply.github.com>
2022-05-16 14:39:58 -0700

    Added calculateWeight and user interaction functionality

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-document.getElementById('generate').onclick = function() {
-
 const planetsArray = [
     //Our solar system's bodies' names and their ratio to Earth's gravity
-    ["Pluto", 0.06],
-    ["Neptune", 1.148],
-    ["Uranus", 0.917],
-    ["Saturn", 1.139],
-    ["Jupiter", 2.64],
-    ["Mars", 0.3895],
-    ["Moon", 0.1655],
-    ["Earth", 1],
-    ["Venus", 0.9032],
-    ["Mercury", 0.377],
-    ["Sun", 27.9],
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
 ];
 
-let select = document.createElement("select");
-    select.name = "planets";
-    select.id = "planets"
+const select = document.getElementById("planets");
+let dropdown = planetsArray.reverse();
 
-for (const planet of planetsArray){
-    var option = document.createElement("option");
-    option.value = planetsArray[i][0];
-    option.text = val.charAt(0).toUpperCase() + val.slice(1);
-    select.appendChild(option);
+planetsArray.forEach(function(item){
+    var opt = item[0];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+});
+function calculateWeight(weight, planetName) {
+    let gravity;
+    for (var i=0; i < planetsArray.length; i++){
+        if(planetName == planetsArray[i][0]){
+        gravity = planetsArray[i][1];
+        }
+    }
+    let resultWeight = weight * gravity;
+    return resultWeight;
 }
-
-var label = document.createElement("label");
-    label.innerHTML = "Choose a Planet: ";
-    label.htmlFor = "planets";
-
-    document.getElementById("container").appendChild(label).appendChild(select);
+function handleClickEvent(e) {
+    let userWeight = document.getElementById("user-weight").value;
+    let planetName = document.getElementById("planets").value;
+    let result = calculateWeight(userWeight, planetName);
+    document.getElementById('output').innerHTML = `If you were on ${planetName}, you would weigh ${result}lbs!`;
 }
-function calculateWeight(weight, planetName){
-let weight = 100;
-for(let i = 0; i < planetsArray.length; i++) {
-    //   console.log("Your weight on" + planetsArray[i][0] + weight * planetsArray[i][1])
-       console.log(`Your weight on ${planetsArray[i][0]} is ${(weight * planetsArray[i][1]).toFixed(2)}`)
-}
-}
+    document.getElementById('calculate-button').addEventListener('click', function(){handleClickEvent()});
